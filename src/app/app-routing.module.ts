2017261ca42a6294e74301c0c1c3689c17dd5caa
User <user@Users-MBP.Dlink>
2019-03-19 17:34:23 +0530

    project 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+// import { Tab4Component } from './tab4/tab4.component';
+// import { Tab5Component } from './tab5/tab5.component';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
